5f8603a55f8fcf296de48b75b244cbbec1fcc57b
Linoy Tsaban <LinoyTsaban@users.noreply.huggingface.co>
2023-09-27 09:29:51 +0000

    Update modified_pipeline_semantic_stable_diffusion.py

--- modified_pipeline_semantic_stable_diffusion.py
+++ modified_pipeline_semantic_stable_diffusion.py
@@ -12,8 +12,8 @@ from diffusers.models import AutoencoderKL, UNet2DConditionModel
 from diffusers.models.attention_processor import AttnProcessor, Attention
 from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
 from diffusers.schedulers import KarrasDiffusionSchedulers
-from diffusers.utils import logging, randn_tensor
-# from diffusers.utils.torch_utils import randn_tensor
+from diffusers.utils import logging
+from diffusers.utils.torch_utils import randn_tensor
 from diffusers.pipelines.pipeline_utils import DiffusionPipeline
 from diffusers.pipelines.stable_diffusion import StableDiffusionPipelineOutput
 # from . import SemanticStableDiffusionPipelineOutput
