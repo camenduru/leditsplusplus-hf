e1756b6886ad9a432484845d7b9d307b5cdba72e
Linoy Tsaban <LinoyTsaban@users.noreply.huggingface.co>
2023-09-27 09:26:21 +0000

    Update pipeline_semantic_stable_diffusion_img2img_solver.py

--- pipeline_semantic_stable_diffusion_img2img_solver.py
+++ pipeline_semantic_stable_diffusion_img2img_solver.py
@@ -12,9 +12,9 @@ from diffusers.models.attention_processor import AttnProcessor, Attention
 from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
 from diffusers.schedulers import DDIMScheduler
 from scheduling_dpmsolver_multistep_inject import DPMSolverMultistepSchedulerInject
-# from diffusers.utils import logging, randn_tensor
-from diffusers.utils import logging
-from diffusers.utils.torch_utils import randn_tensor
+from diffusers.utils import logging, randn_tensor
+# from diffusers.utils import logging
+# from diffusers.utils.torch_utils import randn_tensor
 from diffusers.pipelines.pipeline_utils import DiffusionPipeline
 from diffusers.pipelines.semantic_stable_diffusion import SemanticStableDiffusionPipelineOutput
 
